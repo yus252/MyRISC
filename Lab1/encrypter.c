4ce868d40d3f98fbe9122e174fa9b286840ae9ff
yus252 <yus252@ucsd.edu>
2021-01-25 20:57:50 -0800

    simplified code

--- Lab1/encrypter.c
+++ Lab1/encrypter.c
@@ -10,33 +10,10 @@ unsigned char setBit(unsigned char n, unsigned int p, unsigned int b)
     return (n & ~mask) | ((b << p) & mask); 
 }
 
-unsigned char lfsr(unsigned char tap_seq, unsigned char seed){
-    unsigned char b = 0;
-    unsigned char next = seed >> 1;   
-    
-    if(tap_seq > 0x08) tap_seq = tap_seq & 0x07;
-
-    if(tap_seq == 0x00) b = getBit(seed, 6) ^ getBit(seed,5);
-    else if(tap_seq == 0x01) b = getBit(seed, 6) ^ getBit(seed, 3);
-    else if(tap_seq == 0x02) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 4) ^ getBit(seed, 3);
-    else if(tap_seq == 0x03) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 4) ^ getBit(seed, 1); 
-    else if(tap_seq == 0x04) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 3) ^ getBit(seed, 1); 
-    else if(tap_seq == 0x05) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 3) ^ getBit(seed, 0); 
-    else if(tap_seq == 0x06) b = getBit(seed, 6) ^ getBit(seed, 4) ^ 
-                                 getBit(seed, 3) ^ getBit(seed, 2); 
-    else if(tap_seq == 0x07) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 4) ^ getBit(seed, 3) ^
-                                 getBit(seed, 2) ^ getBit(seed, 1); 
-    else if(tap_seq == 0x08) b = getBit(seed, 6) ^ getBit(seed, 5) ^ 
-                                 getBit(seed, 4) ^ getBit(seed, 3) ^
-                                 getBit(seed, 1) ^ getBit(seed, 0); 
-    
-    setBit(next, 0, (unsigned int) b);
-    return next;
+unsigned char lfsr_next(unsigned char tap, unsigned char seed){
+    seed = seed >> 1;   
+    setBit(next, 0, (unsigned int) tap);
+    return seed;
 }
 
 char * encrypter(char * mem){
@@ -48,12 +25,35 @@ char * encrypter(char * mem){
     count = mem[61];
    
     // step 3
-    unsigned char tap = mem[62];
-    unsigned char state = mem[63];
 
     if(count < 10) count = 10;
     else if(count > 26) count = 26;
-   
+ 
+    // lfsr
+    unsigned char tap = mem[62];
+    unsigned char state = mem[63];
+
+   if(tap > 0x08) tap = tap & 0x07;
+
+    if(tap == 0x00) tap = getBit(seed, 6) ^ getBit(seed,5);
+    else if(tap == 0x01) tap = getBit(seed, 6) ^ getBit(seed, 3);
+    else if(tap == 0x02) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 4) ^ getBit(seed, 3);
+    else if(tap == 0x03) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 4) ^ getBit(seed, 1); 
+    else if(tap == 0x04) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 3) ^ getBit(seed, 1); 
+    else if(tap == 0x05) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 3) ^ getBit(seed, 0); 
+    else if(tap == 0x06) tap = getBit(seed, 6) ^ getBit(seed, 4) ^ 
+                                 getBit(seed, 3) ^ getBit(seed, 2); 
+    else if(tap == 0x07) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 4) ^ getBit(seed, 3) ^
+                                 getBit(seed, 2) ^ getBit(seed, 1); 
+    else if(tap == 0x08) tap = getBit(seed, 6) ^ getBit(seed, 5) ^ 
+                                 getBit(seed, 4) ^ getBit(seed, 3) ^
+                                 getBit(seed, 1) ^ getBit(seed, 0);  
+
     // step 4
     for(i = 0; i < count; i++){
         state = lfsr(tap, state);
