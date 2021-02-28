6ba25b1fa318ef6ff35be1051f4207b4e2ca24dd
Connor Frank <cfrank@ucsd.edu>
2021-02-27 16:40:13 -0800

    basic unit tests for I type instructions

--- Lab2/TestUnit_tb.sv
+++ Lab2/TestUnit_tb.sv
@@ -39,17 +39,70 @@ initial begin
     
 // launch prodvgram in DUT
   #10ns Req = 0;
+
+/*
+  // display all registers 
+  for(int j=0; j<16; j++)
+    $display("%d: %h", j, DUT.RF1.Registers[j]);
+*/
   
   // instr: li 1 
   //   out: r0 = 1
-  #10ns Instruction = 9'b1_000_00001;
+  Instruction = 9'b1_000_00001;
+  #10ns if(DUT.RF1.Registers[0] != 8'b1) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+
+  // instr: addi 1 
+  //   out: r0 = 2
+  Instruction = 9'b1_100_00001;
+  #10ns if(DUT.RF1.Registers[0] != 8'b10) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+  
+  // instr: subi 1 
+  //   out: r0 = 1
+  Instruction = 9'b1_101_00001;
+  #10ns if(DUT.RF1.Registers[0] != 8'b1) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+		
+  // instr: luti 11  
+  //   out: r0 = 64
+  Instruction = 9'b1_110_01011;
+  #10ns if(DUT.RF1.Registers[0] != 8'b1000000) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+  
+  // instr: sb1 0  
+  //   out: r0 = 65
+  Instruction = 9'b1_011_00000;
+  #10ns if(DUT.RF1.Registers[0] != 8'b1000001) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+  
+  // instr: sb0 0  
+  //   out: r0 = 64
+  Instruction = 9'b1_010_00000;
+  #10ns if(DUT.RF1.Registers[0] != 8'b1000000) begin
+			$display("(ERROR) Instruction %b", Instruction);
+			$stop;
+		end
+		
+  // instr: gbi 6  
+  //   out: r0 = 1
+  Instruction = 9'b1_001_00110;
   #10ns if(DUT.RF1.Registers[0] != 8'b1) begin
 			$display("(ERROR) Instruction %b", Instruction);
 			$stop;
 		end
   
-  
-  #20ns $stop;
+  #10ns $stop;
   
 /*  
 // Wait for done flag, then display results
